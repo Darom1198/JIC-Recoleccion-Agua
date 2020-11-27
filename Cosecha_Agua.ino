f1f1403c50c51ef7b2b76d664eb1aece13ae4ec2
Darom1198 <55759304+Darom1198@users.noreply.github.com>
2020-11-26 23:17:35 -0500

    Publicación Oficial
    Versión 1.0 del proyecto.

---
+++ Cosecha_Agua.ino
@@ -0,0 +1,33 @@
+//Pines de Sensores
+const int solenoide = 13;
+const int sensorAgua = A5;
+
+//Un dia en milisegundos
+const long dia = 86400000;
+
+//Declaracion de variable que recibe valor de lectura del sensor de agua
+int lecturaAgua = 0;
+
+void setup() {
+  //Inicializacion de los sensores
+  pinMode(solenoide, OUTPUT);
+  pinMode(sensorAgua, INPUT);
+}
+
+void loop() {
+  //El solenoide se cierra antes de comprobar si existe agua suficiente
+  digitalWrite(solenoide, HIGH);
+  //Lectura de sensor de agua
+  lecturaAgua = analogRead(sensorAgua);
+  if (lecturaAgua > 300) {
+    //Se abre el solenoide
+    digitalWrite(solenoide, LOW);
+    //Se espera que la planta se riegue por 3 minutos
+    delay(180000)
+  } else {
+    //Mantiene solenoide cerrado
+    digitalWrite(solenoide, HIGH)
+  }
+  //El sensor de agua vuelve a verificar despues de un dia
+  delay(day);
+}
